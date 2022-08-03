8e6c766ac124f2e08d1008da7f2cba0ff3a57fbe
hjson <hjson422@gmail.com>
2022-08-03 10:34:01 +0900

    no message

---
+++ src/main/java/BitProject/Greener/domain/dto/BoardsWithUserDTO.java
@@ -0,0 +1,42 @@
+package BitProject.Greener.domain.dto;
+
+import static javax.persistence.EnumType.STRING;
+
+import BitProject.Greener.common.BoardsType;
+import BitProject.Greener.common.RoleType;
+import BitProject.Greener.domain.entity.Boards;
+import BitProject.Greener.domain.entity.UserEntity;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BoardsWithUserDTO {
+
+    private Long boardsId;
+    private String title;
+    private String content;
+    private BoardsType boardsType;
+    private Long userId;
+    private String name;
+    private String email;
+
+    private String nickName;
+
+
+    public static BoardsWithUserDTO convertToDto(Boards boards, UserEntity userEntity){
+        BoardsWithUserDTO instance = new BoardsWithUserDTO();
+        instance.boardsId = boards.getId();
+        instance.title = boards.getTitle();
+        instance.content = boards.getContent();
+        instance.boardsType = boards.getBoardsType();
+        instance.userId = userEntity.getId();
+        instance.name = userEntity.getName();
+        instance.email = userEntity.getEmail();
+        instance.nickName = userEntity.getNickName();;
+
+    }
+}
