e9fa019739aa44df051a665a35278b1e2ddc42a6
hjson <hjson422@gmail.com>
2022-08-18 16:48:53 +0900

    boards List 정렬 방식 변경

--- src/main/java/BitProject/Greener/repository/BoardsRepository.java
+++ src/main/java/BitProject/Greener/repository/BoardsRepository.java
@@ -17,8 +17,11 @@ public interface BoardsRepository extends JpaRepository<Boards, Long> {
     @Query(value = "SELECT b FROM Boards b join fetch b.userEntity")
     List<Boards> findAllWithUser();
 
-    @Query(value = "SELECT b FROM Boards b join fetch b.userEntity join fetch b.category")
+    @Query(value = "SELECT b FROM Boards b join fetch b.userEntity join fetch b.category ORDER BY b.id DESC")
     List<Boards> findAllWithPagination(Pageable pageable);
+
+//    @Query(value = "SELECT b FROM Boards b ORDER BY b.id DESC")
+//    List<Boards> findAllWithPagination(Pageable pageable);
 
 
 }
